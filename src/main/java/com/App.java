aaa0838f6026289eb5654b6c257a117698c51126
‘Gabe <gabe.bretthauer@transport.nsw.gov.au>
2024-07-23 15:52:48 +1000

    Change gradient to singleton

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -17,12 +17,7 @@ public class App {
         int worldZ = 200;
         Elevation terrainGenerator = new Elevation(worldX, worldY, worldZ);
         Ocean oceanGenerator = new Ocean(worldX, worldY, worldZ);
-        Color forestGreen = new Color(63, 84, 41);
-        Color snowyWhite = new Color(222, 221, 220);
-        Color turquiose = new Color(53, 156, 132);
-        Color deepBlue = new Color(30, 53, 66);
-        Gradient terrainGradient = new Gradient(forestGreen, snowyWhite);
-        Gradient oceanGradient = new Gradient(deepBlue, turquiose);
+        Gradient gradient = Gradient.getInstance();
 
         // create terrain maps
         int[][] terrain = terrainGenerator.generate();
@@ -33,15 +28,21 @@ public class App {
         // Create a BufferedImage object
         BufferedImage image = new BufferedImage(worldX, worldY, BufferedImage.TYPE_INT_ARGB);
 
+        // colours for world generation
+        Color forestGreen = new Color(63, 84, 41);
+        Color snowyWhite = new Color(222, 221, 220);
+        Color turquiose = new Color(53, 156, 132);
+        Color deepBlue = new Color(30, 53, 66);
+
         // Set pixel values in the BufferedImage
         for (int x = 0; x < worldX; x++) {
             for (int y = 0; y < worldY; y++) {
                 // draw an ocean if it exists, or a gradiated terrain, otherwise
                 if (ocean[x][y] != 0) {
-                    image.setRGB(x, y, oceanGradient.value(ocean[x][y], worldZ));
+                    image.setRGB(x, y, gradient.value(turquiose, deepBlue, ocean[x][y], worldZ));
                 }
                 else {
-                    image.setRGB(x, y, terrainGradient.value(terrain[x][y], worldZ));
+                    image.setRGB(x, y, gradient.value(forestGreen, snowyWhite, terrain[x][y], worldZ));
                 }
             }
         }
--- src/main/java/com/utilities/Gradient.java
+++ src/main/java/com/utilities/Gradient.java
@@ -3,16 +3,17 @@ package com.utilities;
 import java.awt.Color;
 
 public class Gradient {
+	// singleton instance
+	private static Gradient instance = null;
+    
+    // get singleton class
 
-    Color startColor;
-    Color endColor;
-
-    public Gradient(Color startColor, Color endColor) {
-        this.startColor = startColor;
-        this.endColor = endColor;
+	public static synchronized Gradient getInstance() {
+        if (instance == null) instance = new Gradient();
+		return instance;
     }
 
-    public int value(int n, int maxN) {
+    public int value(Color startColor, Color endColor, int n, int maxN) {
         // Ensure n is within the range [0, maxN]
         n = Math.max(0, Math.min(n, maxN));
         
@@ -20,15 +21,17 @@ public class Gradient {
         float ratio = (float) n / maxN;
 
         // Interpolate between the two colors
-        int red = getColourSection(ratio, "red", 1.04);
-        int green = getColourSection(ratio, "green", 1.03);
-        int blue = getColourSection(ratio, "blue", 0.95);
+        int red = getColourSection(ratio, "red", 1.04, startColor, endColor);
+        int green = getColourSection(ratio, "green", 1.03, startColor, endColor);
+        int blue = getColourSection(ratio, "blue", 0.95, startColor, endColor);
 
         // Return the RGB integer value
         return new Color(red, green, blue).getRGB();
     }
 
-    private int getColourSection(float ratio, String color, Double rate) {
+    private int getColourSection(
+        float ratio, String color, Double rate, Color startColor, Color endColor
+        ) {
         int start = getColourComponent(startColor, color);
         int end = getColourComponent(endColor, color);
         int deviation = (int)(rate * Math.sin(rate * 15 * ratio) * 4);
